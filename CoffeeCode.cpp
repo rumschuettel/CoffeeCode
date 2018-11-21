a74a61351cb74d6e6e0ba330ce7c2d481b5fdefa
Johannes Bausch <johannes.bausch@gmail.com>
2018-11-21 10:11:37 +0100

    initial commit

---
+++ CoffeeCode.cpp
@@ -0,0 +1,141 @@
+﻿#include "CoffeeCode.h"
+
+#include <iostream>
+#include <cstdint>
+#include <vector>
+#include <assert.h>
+
+using std::cout;
+using std::endl;
+
+// compile time integer exponent
+constexpr size_t ipow(const size_t base, const int exp, const size_t result = 1) {
+	return exp < 1 ? result : ipow(base*base, exp / 2, (exp % 2) ? result * base : result);
+}
+// compile time size of base k tuple
+template<const size_t base, const size_t tuple_length>
+struct BaseKSubsets {
+	static constexpr auto count = ipow(base, tuple_length);
+};
+// compile time bitmask with k 1s
+template<const size_t number_of_1s>
+struct Bitmask1s {
+	static constexpr auto mask = (1ull << number_of_1s) - 1;
+};
+
+// monomial
+struct Monomial {
+	using CoefficientT = uint16_t;
+	using ExponentT = uint8_t;
+	static constexpr auto MaxCoefficient = std::numeric_limits<CoefficientT>::max();
+};
+
+// polynomial
+template<const size_t max_exponent>
+struct Polynomial {
+	Monomial::CoefficientT coefficients[max_exponent];
+
+	Polynomial() = default; // zero-initializes coefficients
+
+	void Add(const Monomial::ExponentT exponent) {
+		assert(exponent < max_exponent);
+		coefficients[exponent] ++;
+	}
+	void Add(const Polynomial& other) {
+		for (size_t i = 0; i < max_exponent; i++) {
+			assert(coefficients[i] + other.coefficients[i] < Monomial::MaxCoefficient);
+			coefficients[i] += other.coefficients[i];
+		}
+	}
+};
+
+
+int main()
+{
+	constexpr auto k_sys = 18, k_env = 1, k_tot = k_sys + k_env;
+	CoffeeCode::AdjacencyMatrix<k_sys, k_env> M{ "0111111111110000000100000000000000000010000000000000000001000000000000\
+0000001000000000000000000100000000000000000010000000000000000001000000\
+0000000000001000000000000000000100000000000000000010000000000000000001\
+0000000000011111110000000000010000000000000000001000000000000000000100\
+0000000000000000100000000000000000010000000000000000001000000000000000\
+00010000000" };
+
+	// Calculate lambda and lambda_pre
+	constexpr auto max_exponent = k_tot;
+	std::vector<Polynomial<max_exponent>> lambda(ipow(2, k_tot)), lambda_pre(ipow(2, k_sys));
+
+	// iterate over all U1, U2 and U3 subsets
+	// we do not cache as memory access is potentially slower than counting
+	using VectorT = decltype(M)::RowVectorT;
+	using SubsetT = VectorT::StoreT;
+
+	// could reach numerical limits, hence break condition at end
+
+	for (SubsetT subsetX = 0; ; subsetX++) {
+		for (SubsetT subsetY = 0; ; subsetY++) {
+			// we explicitly allow downcasting, so no {} as initializer; in fact the compiler complains if we do.
+			// note that the types match, though, as subsetX is of type VectorT::StoreT, and the bit operations
+			// keep it within the allowed range.
+			const auto XwoY = VectorT(subsetX & ~subsetY);
+			const auto YwoX = VectorT(subsetY & ~subsetX);
+			const auto XnY = VectorT(subsetX & subsetY);
+
+			const Monomial::ExponentT u1 = static_cast<Monomial::ExponentT>(XwoY.popcount());
+			const Monomial::ExponentT u2 = static_cast<Monomial::ExponentT>(YwoX.popcount());
+			const Monomial::ExponentT u3 = static_cast<Monomial::ExponentT>(XnY.popcount());
+
+			const SubsetT Uidx = (M * (XwoY + YwoX) + (YwoX + XnY)).vec;
+			const SubsetT UAidx = Uidx & Bitmask1s<k_sys>::mask;
+
+			// add monomials
+			assert(u1 + u2 + u3 < max_exponent);
+			const Monomial::ExponentT p_exponent = u1 + u2 + u3;
+  			lambda[Uidx].Add(p_exponent);
+			lambda_pre[UAidx].Add(p_exponent);
+
+			if (subsetY == BaseKSubsets<2, k_sys>::count-1) break;
+		}
+
+		if (subsetX == BaseKSubsets<2, k_sys>::count-1) break;
+	}
+
+	// Calculate lambda_a
+	constexpr auto max_exponent_summed = k_tot;
+	std::vector<Polynomial<max_exponent_summed>> lambda_a(ipow(2, k_sys));
+
+	// build Ulookup
+	const auto MAB = M.AB();
+	using SubsetAT = decltype(MAB)::ColumnVectorT::StoreT;
+	using SubsetBT = decltype(MAB)::RowVectorT::StoreT;
+	for (SubsetAT subsetA = 0; ; subsetA++) {
+		for (SubsetBT subsetB = 0; ; subsetB++) {
+			const auto Ulookup = (MAB * subsetB + subsetA).vec;
+			lambda_a[subsetA].Add(lambda_pre[Ulookup]);
+
+			if (subsetB == BaseKSubsets<2, k_env>::count-1) break;
+		}
+
+		if (subsetA == BaseKSubsets<2, k_sys>::count-1) break;
+	}
+
+	// dump output to Mathematica-readable file
+	cout << "{{";
+	for (const auto& polynomial : lambda) {
+		cout << "{";
+		for (const auto& coefficient : polynomial.coefficients) {
+			cout << static_cast<size_t>(coefficient) << ", ";
+		}
+		cout << "Nothing},";
+	}
+	cout << "Nothing},\n{";
+	for (const auto& polynomial : lambda_a) {
+		cout << "{";
+		for (const auto& coefficient : polynomial.coefficients) {
+			cout << static_cast<size_t>(coefficient) << ", ";
+		}
+		cout << "Nothing},";
+	}
+	cout << "Nothing}}" << endl;
+
+	return 0;
+}
---
+++ CoffeeCode.h
@@ -0,0 +1,164 @@
+﻿// Vector is small and used a lot.
+// For the operations we expect to use a lot we thus pass them by value, not reference.
+
+#pragma once
+
+#include "libpopcnt.h"
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+#include <assert.h>
+#include <type_traits>
+
+namespace CoffeeCode {
+	// automatic selection of smallest-possible integer
+	template<bool raise = true>
+	struct InvalidIntegerType {
+		static_assert(raise, "not enough bits");
+	};
+
+	template<size_t length>
+	using StdStoreT = typename std::conditional <
+		length <= 8,
+		uint8_t,
+		typename std::conditional <
+		length <= 16,
+		uint16_t,
+		typename std::conditional <
+		length <= 32,
+		uint32_t,
+		typename std::conditional <
+		length <= 64,
+		uint64_t,
+		InvalidIntegerType<true>
+		>::type>::type>::type>::type;
+
+	using StdBitT = StdStoreT<1>;
+		
+	// fast popcount implementation
+	constexpr auto __popcount = popcnt64;
+
+
+	// binary vector class
+	template <size_t length>
+	struct Vector {
+		using StoreT = StdStoreT<length>;
+		using BitT = StdBitT;
+
+		// enough bits to store vector?
+		static_assert(length <= 8 * sizeof(StoreT), "StoreT too short");
+
+		// template arguments for future use
+		constexpr static size_t length{ length };
+
+		StoreT vec;
+
+		// default constructors
+		Vector() : vec{0} {};
+		Vector(const StoreT vec) : vec{ vec } {};
+		Vector(const Vector& c) = default;
+		Vector(const std::string& bitstring) : vec{ 0 }
+		{
+			assert(bitstring.length() == length);
+			for (size_t i = 0; i < length; i++) {
+				assert(bitstring[i] == '0' || bitstring[i] == '1');
+				BitT bit = bitstring[i] == '0' ? 0 : 1;
+				vec |= StoreT{ bit } << i;
+			}
+		}
+
+		// get number of 1s
+		inline size_t popcount() const {
+			return __popcount(vec);
+		}
+
+		// addition
+		// pass by value
+		inline Vector operator+(const Vector rhs) const {
+			return vec ^ rhs.vec;
+		}
+		inline Vector& operator+=(const Vector rhs) {
+			vec ^= rhs.vec;
+			return *this;
+		}
+
+		// dot product
+		// pass by value
+		inline BitT operator*(const Vector rhs) const {
+			return __popcount(vec & rhs.vec) & BitT{1};
+		}
+
+		// bit access
+		inline BitT operator[](size_t index) const {
+			return !!(vec & (BitT{1} << index));
+		}
+
+		// to string
+		friend std::ostream& operator<< (std::ostream& stream, const Vector& vector) {
+			for (size_t i = 0; i < length-1; i++)
+				// cast to normal int, otherwise it'll print chars
+				stream << static_cast<uint32_t>(vector[i]) << " ";
+			stream << static_cast<uint32_t>(vector[length - 1]);
+			return stream;
+		}
+	};
+
+
+	// binary matrix class
+	template<size_t row_count, size_t column_count>
+	struct Matrix {
+		using RowVectorT = Vector<column_count>;
+		using ColumnVectorT = Vector<row_count>;
+
+		RowVectorT rows[row_count];
+
+		Matrix() = default;
+		Matrix(const std::string bitstring) {
+			// trim whitespace
+			std::string cleaned{ bitstring };
+			cleaned.erase(std::remove_if(cleaned.begin(), cleaned.end(), std::isspace), cleaned.end());
+			assert(cleaned.length() == row_count * column_count);
+
+			// load into rows
+			for (size_t i = 0; i < row_count; i++)
+				rows[i] = RowVectorT(cleaned.substr(column_count*i, column_count));
+		}
+
+		// dot product
+		// pass by value
+		inline ColumnVectorT operator*(const RowVectorT rhs) const {
+			ColumnVectorT out;
+			size_t i = 0;
+			for (const auto row : rows)
+				out += ColumnVectorT::StoreT{ row * rhs } << i++;
+			return out;
+		}
+
+		// to string
+		friend std::ostream& operator<< (std::ostream& stream, const Matrix& matrix) {
+			for (size_t i = 0; i < row_count - 1; i++)
+				stream << matrix.rows[i] << "\n";
+			stream << matrix.rows[row_count - 1];
+			return stream;
+		}
+	};
+
+
+	// adjacency matrix class
+	template<size_t size_A, size_t size_B>
+	struct AdjacencyMatrix : public Matrix<size_A + size_B, size_A + size_B> {
+		constexpr static size_t size_A{ size_A }, size_B{ size_B };
+
+		using Matrix<size_A + size_B, size_A + size_B>::Matrix;
+		using ABBlockT = Matrix<size_A, size_B>;
+
+		// return AB block
+		ABBlockT AB() const {
+			ABBlockT blockAB;
+			for (size_t i = 0; i < size_A; i++)
+				blockAB.rows[i] = static_cast<typename ABBlockT::RowVectorT>( (*this).rows[i].vec >> size_A );
+
+			return blockAB;
+		}
+	};
+}

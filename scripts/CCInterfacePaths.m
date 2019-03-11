7c7992d14be4f61a325bdbd14161ed068e5b1740
Johannes Bausch <johannes.bausch@gmail.com>
2019-03-11 17:40:19 +0000

    added external paths file

---
+++ scripts/CCInterfacePaths.m
@@ -0,0 +1,10 @@
+(* ::Package:: *)
+
+(* ::Input::Initialization:: *)
+CCRELEASEPATHFULLSOLVER="/home/jkrb2/programming/CoffeeCode/build/release_full_arbch/";
+
+RESULTSPATH="/home/jkrb2/Desktop/GraphStateResults/";
+
+PARALLELBUILDPATH="/home/jkrb2/programming/CoffeeCode/build/";
+PARALLELBUILDSETUPSCRIPT="/home/jkrb2/programming/CoffeeCode/scripts/make-build-dir.sh";
+CCCUSTOMINSTANCEFILE="cc-instance-custom.h";

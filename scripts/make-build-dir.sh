a26aff43bbff5ffd83d44fb1c931777b963f8051
Johannes Bausch <johannes.bausch@gmail.com>
2019-03-11 17:37:58 +0000

    improved CCInterface

--- scripts/make-build-dir.sh
+++ scripts/make-build-dir.sh
@@ -8,4 +8,4 @@ cd $1
 source /home/jkrb2/opt/anaconda5/etc/profile.d/conda.sh
 conda activate gcc8
 
-cmake -D CMAKE_BUILD_TYPE=Release ../..
+cmake -D CMAKE_BUILD_TYPE=Release -D OPTIMIZE_FOR_DEPOLARIZING=ON ../..

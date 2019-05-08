b58b8c56896de6f7a4af5d393cde94ae543dde03
Johannes Bausch <johannes.bausch@gmail.com>
2019-05-08 10:38:42 +0100

    mono- and multivariate poly specified in cc-instance working

--- scripts/CCInterfacePaths.m
+++ scripts/CCInterfacePaths.m
@@ -6,7 +6,8 @@ CurrentDir:=If[$InputFileName!="",DirectoryName[$InputFileName],NotebookDirector
 
 
 (* ::Input::Initialization:: *)
-CCRELEASEPATHFULLSOLVER=ParentDirectory[CurrentDir]<>"/build/release_full_arbch/";
+CCRELEASEPATHFULLSOLVER=ParentDirectory[CurrentDir]<>"/build/release_full/";
+CCRELEASEPATHFULLSOLVERARBCH=ParentDirectory[CurrentDir]<>"/build/release_full_arbch/";
 
 PARALLELBUILDPATH=ParentDirectory[CurrentDir]<>"/build/";
 PARALLELBUILDSETUPSCRIPT=CurrentDir<>"/make-build-dir.sh";
@@ -15,6 +16,7 @@ RESULTSPATH=$HomeDirectory<>"/Desktop/GraphStateResults/";
 
 
 Print["Path to precompiled full solvers: "<>CCRELEASEPATHFULLSOLVER];
+Print["Path to precompiled full solvers for all channels: "<>CCRELEASEPATHFULLSOLVERARBCH];
 Print["Path to create parallel solver subdirectories: "<>PARALLELBUILDPATH];
 Print["Script how to setup new parallel solver subdirectories: "<>PARALLELBUILDSETUPSCRIPT];
 Print["Results path, can be used as RESULTSPATH (can be ignored): "<>RESULTSPATH];

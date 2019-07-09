8f7a5d3e25fd6d098fa0a30f5d28a4fe634f5665
Johannes Bausch <johannes.bausch@gmail.com>
2019-07-09 18:12:02 +0100

    postprocessing script with numexpr and degradability filter

--- scripts/postprocess-3dplot.py
+++ scripts/postprocess-3dplot.py
@@ -1,40 +1,39 @@
 #!/usr/bin/env python
 
 import numpy as np
-import numpy.polynomial.polynomial as poly
-from scipy.special import xlogy
+import numexpr as ne
 import tarfile, gzip, json
 import re, math
 import os, sys, multiprocessing
+import inspect
 
 
 LOG2INV = 1./np.log(2.0)
 FLOAT_TYPE = np.float64
 
-# calculate shannon entropy from a lambda vector
-# fac can be used to rescale the terms for lambda_a
-# qs is a np array with last dimension 4
-def shannon_entropy_from_lambda(lambda_list, kSys, fac, qs):
-    entropy = np.zeros(qs.shape[:-1], dtype=FLOAT_TYPE)
-    global_mult = 0.
+# calculate shannon entropy numexpr
+def shannon_entropy_numexpr_from_lambda(lambda_list, kSys, fac, qs=["(1.-q1-q2-q3)","q1","q2","q3"]):
+    entropy = ""
+    global_mult = 0
 
     for mult, poly in lambda_list:
         if len(poly) == 0:
             continue
 
-        term = np.zeros(entropy.shape, dtype=FLOAT_TYPE)
+        term = []
         for [coeff, [e1, e2, e3]] in poly:
-            term += (
-                fac * coeff * np.prod(qs ** (kSys - e1 - e2 - e3, e1, e2, e3), axis=-1)
-            )
+            term.append(f"{fac} * {coeff} * ({qs[0]}**{(kSys - e1 - e2 - e3)}) * ({qs[1]}**{e1}) * ({qs[2]}**{e2}) * ({qs[3]}**{e3})")
 
-        # xlogy calculates x log(y), thus allowing for 0. log(0.) to be evaluated correctly
-        entropy -= mult * xlogy(term, term) * LOG2INV
+        term = "(" + "+".join(term) + ")"
+
+        # numexpr optimizes this call to remove double-evaluation
+        entropy += f"-{mult} * where({term}==0,0,{term}*log({term})*{LOG2INV})"
 
         # accumulate multiplicity
         global_mult += mult
 
     return entropy, global_mult
+
 
 
 if __name__ == "__main__":
@@ -87,25 +86,26 @@ if __name__ == "__main__":
 
     # build q lookup table to clone
     # this is not meant to be memory-efficient, otherwise we'd create them on the fly
-    qs_ref = np.array(
-        [
-            [
-                [
-                    [1 - q1 - q2 - q3, q1, q2, q3]
-                    for q1 in np.arange(MIN, MAX + STEP, STEP)
-                ]
-                for q2 in np.arange(MIN, MAX + STEP, STEP)
-            ]
-            for q3 in np.arange(MIN, MAX + STEP, STEP)
-        ],
-        dtype=FLOAT_TYPE
-    )
+    def qs_ref():
+        for q1 in np.arange(MIN, MAX+STEP, STEP):
+            for q2 in np.arange(MIN, MAX+STEP, STEP):
+                for q3 in np.arange(MIN, MAX+STEP, STEP):
+                    if q1+q2+q3 > 1:
+                        continue
+
+                    # antidegradability condition
+                    if 2*((1-q1-q2)**2 + (q1-q2)**2 + (q1+q2)**2 + (1-q1-q2-2*q3)**2) - 16*math.sqrt(q1*q2*q3*(1-q1-q2-q3)) < 2:
+                        continue
+
+                    yield [q1, q2, q3]
+
+    q1,q2,q3 = np.transpose(np.array(list(qs_ref())))
 
     # best CI
-    best_ci = np.zeros(qs_ref.shape[:-1], dtype=FLOAT_TYPE)
+    best_ci = np.zeros(q1.shape, dtype=FLOAT_TYPE)
     best_ci.fill(-10**9)
     # best graph, we store the adjacency matrix
-    best_graph = np.zeros(best_ci.shape, dtype=np.uint16)
+    best_graph = np.zeros(q1.shape, dtype=np.uint16)
 
 
     # iterate over files in INFILE
@@ -120,7 +120,7 @@ if __name__ == "__main__":
                 len(adjm_matches) == 1
             ), "invalid archived name, must encode adjm in 01 format as in 0110.json.gz"
             KTOT = math.sqrt(len(adjm_matches[0]))
-            assert KTOT.is_integer()
+            assert KTOT.is_integer(), "adjacency matrix not square"
             KTOT = int(KTOT)
             KENV = KTOT - KSYS
             assert KENV > 0 and KENV <= KSYS, "0 < kEnv <= kSys not satisfied"
@@ -133,12 +133,15 @@ if __name__ == "__main__":
             with archive.extractfile(file_meta) as file:
                 content = json.load(gzip.GzipFile(fileobj=file))
 
-            S, mult = shannon_entropy_from_lambda(
-                content["lambda"], KSYS, 1.0, qs_ref
+            S_expr, mult = shannon_entropy_numexpr_from_lambda(
+                content["lambda"], KSYS, 1.0
             )
-            S_a, mult_a = shannon_entropy_from_lambda(
-                content["lambda_a"], KSYS, 2.0 ** -KENV, qs_ref
+            S_expr_a, mult_a = shannon_entropy_numexpr_from_lambda(
+                content["lambda_a"], KSYS, 2.0 ** -KENV
             )
+
+            S = ne.evaluate(S_expr)
+            S_a = ne.evaluate(S_expr_a)
             ci = (S_a - S) * (1./np.log2(mult_a))
 
             # update best ci and best graph
@@ -150,6 +153,6 @@ if __name__ == "__main__":
         os.path.join(PATH_THISFILE, args.outfile) if args.outfile else f"{INFILE}-best.npz"
     )
     print(f"saving best ci and graphs in {OUTFILE}")
-    np.savez_compressed(OUTFILE, ci=best_ci, graph=best_graph)
+    np.savez_compressed(OUTFILE, params=[MIN, MAX, STEP], qs_gen=inspect.getsource(qs_ref), ci=best_ci, graph=best_graph)
 
     print("done")
